66a2d2304b3aa2e30593912a295cbc83bf88f06a
JacksonKaunismaa <jackkaunis@gmail.com>
2022-03-11 16:34:42 -0500

    added script to combine data sources into 1 csv

---
+++ generate_csv.py
@@ -0,0 +1,31 @@
+import glob
+import csv
+import librosa
+import sys
+
+
+endings = "[m4a|opus|wav|mp3]"
+
+def get_entries(directory):
+    files = glob.glob(f"./{directory}/*.{endings}")
+    entries = []
+    for f in files:
+        try:
+            entries.append((f,librosa.get_duration(f)))
+        except:
+            raise
+    return entries
+
+
+
+def main():
+    all_entries = []
+    for dir_name in sys.argv[1:]:
+        all_entries += get_entries(dir_name)
+    with open("out.csv", "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["location", "duration"])
+        writer.writerows(all_entries)
+
+if __name__ == "__main__":
+    main()
