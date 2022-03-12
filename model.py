f9216d225eabc0904abc23bfecc6232fdb0e83c5
JacksonKaunismaa <jackkaunis@gmail.com>
2022-03-12 06:04:57 -0500

    added multiscale stuff, now to fix the (very many) bugs

--- model.py
+++ model.py
@@ -85,16 +85,37 @@ class Layer(nn.Module):
         x_freq = x_freq + x_freq_res
         return [x_time, x_freq]
 
+class FeatureExtraction(nn.Module):
+    def __init__(self, num_mels, time_steps, n_layers):
+        super().__init__()
+        # Input layers
+        self.freq_extract = nn.GRU(time_steps, time_steps, batch_first=True, bidirectional=True)
+        self.time_extract = nn.GRU(num_mels, num_mels, batch_first=True, bidirectional=True)
 
-class MelNet(nn.Module):
-    def __init__(self, dims, n_layers, num_classes, n_mixtures=10):
+    def forward(self, spectrogram):
+        # Shift the inputs left for time-delay inputs
+        # spectrogram: (batch_size, time, freq)
+
+        N,T,F = spectrogram.size()
+        freq_input = spectrogram.transpose(1, 2).contiguous()
+
+        freq_features, _ = self.freq_extract(freq_input)   # (batch, freq, 2*time_steps)
+        time_features, _ = self.time_extract(spectrogram)  # (batch, time, 2*freq_steps)
+
+        freq_features = freq_features.transpose(1,2).contiguous().view(-1, T, 2*F)
+
+        return torch.cat((time_features, freq_features), 1)
+
+
+class MelNetTier(nn.Module):
+    def __init__(self, dims, n_layers, n_mixtures=10):
         super().__init__()
         # Input layers
         self.freq_input = nn.Linear(1, dims)
         self.time_input = nn.Linear(1, dims)
 
-        self.time_embeds = nn.Embedding(num_classes, dims)
-        self.freq_embeds = nn.Embedding(num_classes, dims)
+        self.time_cond = nn.Linear(1, dims)
+        self.freq_cond = nn.Linear(1, dims)
 
         # Main layers
         self.layers = nn.Sequential(
@@ -103,22 +124,21 @@ class MelNet(nn.Module):
 
         # Output layer
         self.fc_out = nn.Linear(2 * dims, 3 * n_mixtures)
-
+        self.n_mixtures = n_mixtures
         # Print model size
-        self.num_params()
+        #self.num_params()
 
-    def forward(self, x, cond):
+    def forward(self, x, cond, noise):
         # Shift the inputs left for time-delay inputs
         x_time = F.pad(x, [0, 0, -1, 1, 0, 0]).unsqueeze(-1)
         # Shift the inputs down for freq-delay inputs
         x_freq = F.pad(x, [0, 0, 0, 0, -1, 1]).unsqueeze(-1)
 
-        time_cond = self.time_embeds(cond)
-        freq_cond = self.freq_embeds(cond)
+        cond = cond.unsqueeze(-1) # add dimension of 1 to be able to do the linear layer
 
         # Initial transform from 1 to dims
-        x_time = self.time_input(x_time) + time_cond
-        x_freq = self.freq_input(x_freq) + freq_cond
+        x_time = self.time_input(x_time) + self.time_cond(cond)
+        x_freq = self.freq_input(x_freq) + self.freq_cond(cond)
 
         # Run through the layers
         x = (x_time, x_freq)
@@ -126,28 +146,92 @@ class MelNet(nn.Module):
 
         # Get the mixture params
         x = torch.cat([x_time, x_freq], dim=-1)
-        params = self.fc_out(x)
-        return params
+        mu, sigma, pi = torch.split(self.fc_out(x), self.n_mixtures, axis=-1)
+
+        sigma = torch.exp(sigma)
+        pi = torch.softmax(dim=-1)
+        return mu, sigma, pi
+
+    def forward_sample(self, x, cond, noise):
+        mu, sigma, pi = self.forward(x, cond, noise)
+        mixture = torch.argmax(pi, dim=-1)  # max sampling probably bad because gradients are 0
+        return mu[mixture] + sigma[mixture]*noise
+
+class MelNet(nn.Module):
+    def __init__(self, dims, layer_sizes, num_classes, num_mels, time_steps, directions, n_mixtures=10):
+        super().__init__()
+        self.class_embeds = nn.Embedding(num_classes, 1) # sus
+        self.tiers = [MelNetTier(dims, n_layers) for n_layers in layer_sizes]
+
+        M, T = num_mels, time_steps
+        self.feature_tier = []
+        for d in directions:
+            if d == 1:
+                T //= 2
+            else:
+                M //= 2
+            self.feature_tier.append(FeatureExtraction(M,T,2))
+
+        #   def __init__(self, num_mels, time_steps, n_layers, n_mixtures=10):
+        # Print model size
+        self.directions = directions
+        self.num_params()
+
+    def split(self, x, dim):
+        even = torch.arange(x.shape[dim]//2)*2
+        odd = even + 1
+        if dim == 1:
+            return x[:, even, :], x[:, odd, :]
+        else:
+            return x[:, :, even], x[:, :, odd]
+
+    def interleave(self, x1, x2, dim):
+        interleaved = torch.repeat_interleave(x1, 2, dim=dim)
+        indices = 1+torch.arange(x1.shape[dim]//2)*2
+        if dim == 1:
+            interleaved[:,indices,:] = x2
+        else:
+            interleaved[:,:,indices] = x2
+        return interleaved
+
+    def forward(self, x, cond, noise):
+        def multi_scale(x, g):
+            if g == 0:
+                return self.tiers[0].forward_sample(x, self.class_embeds(cond), noise)
+            else:
+                dim = self.directions[g]
+                x_g, x_g_prev = self.split(x, dim)
+                x_pred_prev = multi_scale(x_g_prev, g-1)
+                prev_features = self.feature_tier[g](x_pred_prev)
+                if g == len(self.tiers)-1:
+                    return self.tiers[g].forward(x_g, prev_features, noise)
+                else:
+                    x_pred = self.tiers[g].forward_sample(x_g, prev_features, noise)
+                    return self.interleave(x_pred, x_pred_prev, dim)
+        return multi_scale(x, len(self.tiers)-1)
 
     def num_params(self):
         parameters = filter(lambda p: p.requires_grad, self.parameters())
         parameters = sum([np.prod(p.size()) for p in parameters]) / 1_000_000
         print('Trainable Parameters: %.3fM' % parameters)
 
-if __name__ == "__main__":
+def main():
     batchsize = 4
-    timesteps = 10
-    num_mels = 8
-    dims = 512
-    n_layers = 5
+    timesteps = 100
+    num_mels = 256
+    dims = 256
 
-    model = MelNet(dims, n_layers, 1)
+    #def __init__(self, dims, layer_sizes, num_classes, n_mixtures=10):
+    model = MelNet(dims, [4,3,2], 2, num_mels, timesteps, [1,2,2]) # split on freq(highest tier), then freq(mid tier), then time(unconditional tier)
 
     x = torch.ones(batchsize, timesteps, num_mels)
-    z = torch.zeros((1), dtype=torch.int64)
+    z = torch.ones((1), dtype=torch.int64)
 
     print("Input Shape:", x.shape)
-
-    y = model(x, z)
+    noise = torch.normal(mean=torch.zeros(batchsize))
+    y = model(x, z, noise)
 
     print("Output Shape", y.shape)
+
+if __name__ == "__main__":
+    main()
