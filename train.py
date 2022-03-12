cd3fc4d41f3a1f3df5afaeabcce3d5a6bd71561d
tracyqian <57376402+tracyqian0720@users.noreply.github.com>
2022-03-11 23:26:47 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,28 @@
 import model
 import data
 import tqdm
+import torch
+import torch.nn as nn
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.random as npr
+from torch.autograd import Variable
 
-EPOCHS = 128
-DIMS = 256
-N_LAYERS = 6
 
+def train(args_idk_what_yet):
+  EPOCHS = 128
+  DIMS = 256
+  N_LAYERS = 6
+  learning_rate = 1e-5
 
-
-
-
-for i in tqdm(range(EPOCHS)):
+  #criterion = nn.CrossEntropyLoss()
+  optimizer = torch.optim.Adam(cnn.parameters(),lr=learning_rate)
+  train_loss_list = np.zeros(EPOCHS)
+  valid_loss_list = np.zeros(EPOCHS)
+  
+  model = MelNet(DIMS, N_LAYERS)
+  
+  for i in tqdm(range(EPOCHS)):
+    
+ 
